7dcb7c31784440912bf9df8b3819d4c6f94e8a87
Joe Polny <joepolny@gmail.com>
2025-06-14 19:56:29 -0400

    wip: bench.rb

---
+++ bench.rb
@@ -0,0 +1,23 @@
+def run(cmd)
+  system(cmd, out: $stdout, err: :out)
+end
+
+target_dir = "#{__dir__}/target/release"
+go_file = 'data/transactions/logic/rust.go'
+
+run "rust2go-cli --src src/lib.rs --dst #{go_file}"
+
+file_content = File.read(go_file)
+
+file_content.sub!('package main', 'package logic')
+file_content.sub!(
+  'manually.',
+  "manually.\n\n#cgo LDFLAGS: -L#{target_dir} -lgo_algorand"
+)
+
+File.open(go_file, 'w') { |file| file.puts file_content }
+
+run 'cargo build --release'
+run 'cargo build --manifest-path test/wasm/fibo/Cargo.toml --target wasm32-unknown-unknown --release'
+run 'wamrc --size-level=3 --bounds-checks=1 -o fibo.aot test/wasm/fibo/target/wasm32-unknown-unknown/release/fibo.wasm'
+run 'go test ./data/transactions/logic -bench=Fibo'
